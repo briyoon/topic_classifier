4dab8cc19743c9277fc815552068131fe01a09df
Estefan <eagonzales2045@outlook.com>
2023-03-04 14:14:33 -0700

    adding split

--- regression_testing.py
+++ regression_testing.py
@@ -11,23 +11,27 @@ learning_rate = 0.01
 total_iterations = 1_000
 
 # can specify rows and label path
-[x, y, c] = get_training_data(training_path=train_path)
+[x, y, c] = get_training_data(training_path=train_path, rows=1_000)
+
+x_train = x[0:500, :]
+y_train = y[0:500, :]
+
+x_test = x[500:1000, :]
+y_test = y[500:1000, :]
 
 [w, t] = lg_fit(learning_rate=learning_rate,
                 penalty=penalty_term,
                 max_iterations=total_iterations,
-                examples=x, target=y, classes=c,
+                examples=x_train, target=y_train, classes=c,
                 show_time=True)
 
-x_test = x[3_000:5_000, :]
-y_test = y[3_000:5_000, :]
 [acc, confusion_mat] = lg_test(x_test, y_test, w, c)
 result_str = get_result_str(learning_rate=learning_rate,
                             penalty=penalty_term,
                             iterations=total_iterations,
                             accuracy=acc,
-                            test_size=len(x),
-                            train_size=len(x_test),
+                            test_size=len(y_test),
+                            train_size=len(y_train),
                             t=t)
 
 # can ignore w, default is none
