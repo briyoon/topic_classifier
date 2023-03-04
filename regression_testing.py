7eac92103d6078acb9b0c33411e2095e2fe4c910
Estefan Gonzales <eagonzales2045@outlook.com>
2023-03-04 15:20:05 -0700

    Ready for robust testing

--- regression_testing.py
+++ regression_testing.py
@@ -3,33 +3,34 @@ from logistic_regression import *
 
 train_path = "/Users/estefan/Desktop/cs429529-project-2-topic-categorization/training.csv"
 
-penalty_term = 0.01
 learning_rate = 0.01
 total_iterations = 1_000
 
+train_size = 4_500
 # can specify rows and label path
-[x, y, c] = get_training_data(training_path=train_path, rows=1_000)
+[x, y, c] = get_training_data(training_path=train_path)
 
-x_train = x
-y_train = y
+for i in range(1, 10):
+    penalty_term = i ** (10 ** -3)
+    indices = np.random.permutation(x.shape[0])
+    train, test = indices[:train_size], indices[train_size:]
+    x_train, x_test = x[train, :], x[test, :]
+    y_train, y_test = y[train, :], y[test, :]
 
-x_test = x
-y_test = y
+    [w, t] = lg_fit(learning_rate=learning_rate,
+                    penalty=penalty_term,
+                    max_iterations=total_iterations,
+                    examples=x_train, target=y_train, classes=c,
+                    show_time=True)
 
-[w, t] = lg_fit(learning_rate=learning_rate,
-                penalty=penalty_term,
-                max_iterations=total_iterations,
-                examples=x_train, target=y_train, classes=c,
-                show_time=True)
+    [acc, confusion_mat] = lg_test(x_test, y_test, w, c)
+    result_str = get_result_str(learning_rate=learning_rate,
+                                penalty=penalty_term,
+                                iterations=total_iterations,
+                                accuracy=acc,
+                                test_size=len(y_test),
+                                train_size=len(y_train),
+                                t=t)
 
-[acc, confusion_mat] = lg_test(x_test, y_test, w, c)
-result_str = get_result_str(learning_rate=learning_rate,
-                            penalty=penalty_term,
-                            iterations=total_iterations,
-                            accuracy=acc,
-                            test_size=len(y_test),
-                            train_size=len(y_train),
-                            t=t)
-
-# can ignore weights or confusion matrix, default is none
-record_test_result(result_str, w, confusion_mat)
+    # can ignore weights or confusion matrix, default is none
+    record_test_result(result_str, w, confusion_mat)
