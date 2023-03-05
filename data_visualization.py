7b116602cda45683f3897c8b8807c1ca6c54c5c6
Estefan Gonzales <eagonzales2045@outlook.com>
2023-03-05 11:19:02 -0700

    adding confusion matrix plotting

--- data_visualization.py
+++ data_visualization.py
@@ -1,11 +1,38 @@
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
-data_path = 'results/CWV1.1_2023-03-03T21:45:10.505296_0.01_0.01_1000.csv'
 
-data = pd.read_csv(data_path, delimiter=',')
-x = range(0, len(data.iloc[0, :]))
+def plot_confusion_matrix(matrix):
+    # hard coded for our classes from 1 to 20
+    labels = list(range(1, 21))
 
-for row in range(0, 19):
-    plt.scatter(x, data.iloc[row, :], alpha=0.3)
-plt.show()
+    fig, ax = plt.subplots(figsize=(7.5, 7.5))
+    # show matrix, use colorbar for legend
+    cax = ax.matshow(matrix, cmap=matplotlib.colormaps['Oranges'], alpha=0.95, interpolation='nearest')
+    fig.colorbar(cax)
+
+    axis_range = np.arange(len(labels))
+    ax.xaxis.set_label_position('top')
+    ax.set_xticks(axis_range)
+    ax.set_yticks(axis_range)
+
+    ax.set_xticklabels(labels, fontsize=8)
+    ax.set_yticklabels(labels, fontsize=8)
+    ax.xaxis.tick_top()
+
+    plt.xlabel('Actual', fontsize=20)
+    plt.ylabel('Predicted', fontsize=20)
+    plt.show()
+
+
+def plot_average_class_weights(weights):
+    x = range(0, len(weights[0]))
+    plt.scatter(x, weights[0], alpha=0.3)
+
+
+conf_mat_path = '/Users/estefangonzales/Desktop/CourseWork/2023/CS429/P2-NBLG/confusion_matrices'
+path = f'{conf_mat_path}/CMAT_20.npy'
+data = np.load(path)
+plot_confusion_matrix(data)
