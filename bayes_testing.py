d3cca2cf9eb274cc643a4301fcbd1c19078090df
Cyrus <61042997+cyrusmcc@users.noreply.github.com>
2023-03-18 16:47:38 -0600

    Bayes testing with multiple beta values

--- bayes_testing.py
+++ bayes_testing.py
@@ -3,11 +3,13 @@ import pandas as pd
 import csv
 import pickle
 import naive_bayes as nb
+from sklearn.model_selection import train_test_split
 
 import os
 
 # Read the csv file
 training_df = pd.read_csv('resources/training.csv')
+training_df, validation_df = train_test_split(training_df, test_size=0.2)
 # testing_df = pd.read_csv('resources/testing.csv')
 
 vocab_df = nb.get_vocab_df()
@@ -25,17 +27,66 @@ label_word_freqs = nb.get_label_word_freqs(
 print(label_word_freqs[1][0])
 
 # classify doc
-priors, cond_probs = nb.train_naive_bayes(label_df, num_docs, num_words, alpha, label_word_freqs) 
-new_doc = training_df.iloc[100, 1:-1] # just for testing
-classification = nb.naive_bayes_classify(training_df.iloc[100, 1:-1], priors, cond_probs)
-print("actual label: ", training_df.iloc[100][-1], "pred label: ", classification)
-
-
+priors, cond_probs = nb.train_naive_bayes(
+    label_df, num_docs, num_words, alpha, label_word_freqs)
+new_doc = training_df.iloc[100, 1:-1]  # just for testing
 test_pred_df = pd.DataFrame(columns=['id', 'pred'])
 
 for i, doc in training_df.iterrows():
     to_classify = doc[1:-1]
-    classification = nb.naive_bayes_classify(to_classify, priors, cond_probs)
-    test_pred_df = pd.concat([test_pred_df, pd.DataFrame({'id': doc[0], 'pred': classification}, index=[doc[0]])])
+    classification = nb.naive_bayes_classify(
+        to_classify, priors, cond_probs, num_labels)
+    test_pred_df = pd.concat([test_pred_df, pd.DataFrame(
+        {'id': doc[0], 'pred': classification}, index=[doc[0]])])
 
 test_pred_df.to_csv('nb_pred.csv', index=False, sep='\t')
+
+# validation
+validation_pred_df = pd.DataFrame(columns=['id', 'pred'])
+
+for i, doc in validation_df.iterrows():
+    to_classify = doc[1:-1]
+    classification = nb.naive_bayes_classify(
+        to_classify, priors, cond_probs, num_labels)
+    validation_pred_df = pd.concat([validation_pred_df, pd.DataFrame(
+        {'id': doc[0], 'pred': classification}, index=[doc[0]])])
+
+print('accuracy: ', nb.find_accuracy(
+    validation_df, validation_pred_df) * 100, '%')
+
+betas = np.linspace(0.00001, 1, num=4)
+
+for beta in betas:
+    alpha = 1 + beta
+    priors, cond_probs = nb.train_naive_bayes(
+        label_df, num_docs, num_words, alpha, label_word_freqs)
+    validation_pred_df = pd.DataFrame(columns=['id', 'pred'])
+
+    for i, doc in validation_df.iterrows():
+        to_classify = doc[1:-1]
+        classification = nb.naive_bayes_classify(
+            to_classify, priors, cond_probs, num_labels)
+        validation_pred_df = pd.concat([validation_pred_df, pd.DataFrame(
+            {'id': doc[0], 'pred': classification}, index=[doc[0]])])
+
+betas = np.linspace(0.00001, 1, num=10)
+accuracies = []
+
+for beta in betas:
+    alpha = 1 + beta
+    priors, cond_probs = nb.train_naive_bayes(
+        label_df, num_docs, num_words, alpha, label_word_freqs)
+    validation_pred_df = pd.DataFrame(columns=['id', 'pred'])
+
+    for i, doc in validation_df.iterrows():
+        to_classify = doc[1:-1]
+        classification = nb.naive_bayes_classify(
+            to_classify, priors, cond_probs, num_labels)
+        validation_pred_df = pd.concat([validation_pred_df, pd.DataFrame(
+            {'id': doc[0], 'pred': classification}, index=[doc[0]])])
+
+    accuracies.append(nb.find_accuracy(
+        validation_df, validation_pred_df) * 100)
+
+    print('beta: ', beta, 'accuracy: ', nb.find_accuracy(
+        validation_df, validation_pred_df) * 100, '%')
